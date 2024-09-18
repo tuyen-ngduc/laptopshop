c1a414d0f0ae46a1dea1e87e299acdaef43e1f57
tuyenngduc <nguyenductuyen1202@gmail.com>
2024-09-18 20:23:53 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,21 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RestController
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaptopshopApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello moi nguoi nhe";
 	}
 
 }
